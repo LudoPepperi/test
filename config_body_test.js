8aec984fe299f2db39ae1a0381694e0dca4a0c71
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-01-26 09:57:45 +0100

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -1,16 +1,16 @@
-var Transaction = 'Sales Order';
-var Catalog='Default Catalog';
+var Transaction = 'Commande B2B';
+var Catalog='B2B'
 var blocks_config = {
     'free_shipping': {
         text: "",
         field: "TSAFreeShipping",
-        svg:"https://storage.pepperi.com/General/Icons/truck.svg",
+        svg:"https://storage.pepperi.com/General/Icons/truck.svg"
     },
     'account_balance': {
         text: "Point",
         field: "TSACreditLimit",
         measure_unit: "Points",
-        svg:"https://storage.pepperi.com/General/Icons/balance.svg",
+        svg:"https://storage.pepperi.com/General/Icons/balance.svg"
     },
     'active-order':
     {
@@ -19,82 +19,151 @@ var blocks_config = {
             text: "Sous-Total",
             field: "SubTotal"
         }, {
-            text: "Total Quantité",
+            text: "Total Quantit�",
             field: "QuantitiesTotal"
         }, {
             text: "Total HT",
             field: "GrandTotal"
-        },]
-    },
+        }]
+    }
+    ,
     'submitted_orders': {
-        name: "Dernière commande",
+        name: "Derni�re commande",
         statuses: ["2"],
         table: ["ActionDateTime", "InternalID"]
-    },
+    }
 }
-var Brands = []
+var Brands = [
+    {
+        bigImage: true,
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Face%20Serums%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/8.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Hair%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/7.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Dry%20Shampoo%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/16.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Co%20Wash%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/17.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Treatments%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/18.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Treatments%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/19.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Face%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/23.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/6.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22New%20Arrivals%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/22.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%22%7D%22',
+        img: 'https://storage.pepperi.com/Beauty_demo/10.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22New%20Arrivals%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/11.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%228cf1f19b-9815-40ea-becf-6eced87d910e%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/12.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22fcef195d-fcdd-4ce6-b6e7-f506bc92c46c%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/13.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Face%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/21.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Masks%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%222a7c613e-1e67-4497-af60-ae8a6633487a%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/15.jpg',
+
+    }]
 //Promotions block
 var Promotions = [
     {
         title: "MOANA",
         buttonText: "Shop Now",
-        link: 'Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Paul%20Pitchell%22%5D%7D%5D',
-        image: 'https://uca20d97102c9811d05719a6e403.previews.dropboxusercontent.com/p/thumb/ABB6K9WuXk55WMP8YgeCGDyakewQ3OQTVCnyqdV5eW1Vy7OB6CqsCaE6l6ZOlNIelI6BpkA-RUPsCB1vt2dWSwO688EOsjnTQ6WUP0EfJ768uqC7FAYVEVYCVl3CFy7RQfp3JtaOh6V8f4tSh5qQdHqBpeIQYASfW312mc1_16OGnSbYnHhv1xdUpB2X3y1MAVJaRnr6euNf-gbykoY5po2YYkDQmIpNxd2rL4HllkmRJ3UjUic0OoR_xNlEyqP7b455RyYAuXaAc8OwgjicXrGHWZ8XTDBmi9UNRxfqRpQ15KrAL2z2hISq8R3RppBeDD4N2lYx9mLAc4T41wXhQhoH07HiyxWKDbsNy3QPUnnbIQ/p.jpeg?fv_content=true&size_mode=5'
-    }, 
+        link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Paul%20Pitchell%22%5D%7D%5D',
+        image: 'https://uce5b188caa1b887b7857c2a01c6.previews.dropboxusercontent.com/p/thumb/ABBiFjjJwlKYkBX2EqpkPz1QdZzfFnuWEMDhaBdpzoxyy8-7k5SofBPLdJh_48eWpyRSZQT5yMqHA042FIx7_S0Sm__yhaCXFKEaX97oEUfocJBzm7w2O4KXZIk2n4ivfzCR88g5tLWgMkAFhqYISyixAGtWc7NuAg0MFhzcj6EbECNDOjhj3cPisKCi4CvsesEHKugMY3f-53ue4Kmc1vBW8tZRvlSwjASvlpxH8pJleeJqWMJ7dSDgoBBVGxi98ZmFscoeovsyacs-N296gFangL5gH3z_feyiEOQ76Dv6PdSwdwJhZt5-fsm0-kCUCLXUzOH3C53X1Tz6-KPYB9yJg_d0bXKjkAvKcaFxhO06ug/p.jpeg?size=178x178&size_mode=1'
+    },
     {
         title: "DELIRIUM FLORAL",
         buttonText: "Shop Now",
-        link: 'Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
-        image: 'https://uce6ee4a0ff91b6e52cdf4f4f028.previews.dropboxusercontent.com/p/thumb/ABDS_JOdNT2_aNfP3PLvesxwtV5AvRvtrrcbgxuFdHQ3gu79LHuh5k2tIHZ_o7-MV-Kc3kS9FAY8_hzxtS6ROivOZf1p7Mzi7qcTN5pDxiM4x1GJ8k2Ws4h_OySo3dgp_wiSti9jDAfY3kND3S6LWXATgGx2QvYjmDASdpBCbqml8xMszcLXx7JPk7gQtQ3RRX4fuszr40n2nzGtpBwGLJck03s7L5TYJh0Bdvj5-KKXfrCvcyrQMLC8ttJ8e9YkBGlfnqXRtwnO3YU1YlJ51hAXRXVZJ0_MGXwRn-jKtkeJZV4vmhDNA_Q9Hl4nVlv2po4Z8S83cl_Fb6l8W2Kz5g1erVeVuR6cghi84fKGvbBFug/p.png?fv_content=true&size_mode=5'
+        link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
+        image: 'https://uca28b6d0df468f95c36bc842719.previews.dropboxusercontent.com/p/thumb/ABCaYaeBbe_2AjrIDtYvpl--AI9XGkHcSS5YCA7FJUW0w6YgbBFTn0zwKKSjowOmYw-ux9IrSUkU2u4LBtYUitK-rjxOwNDenTmE9hyR9RhlUdgANMPf-AWjGd-fQfH9smcicNOKOn9WzHqlmt2q8fApOPiZC3JhtqGSoI7Hy7SHB4V1WPPumjht0cBpUEARVO1jr91tykB9_rdtPqb82sVA4OwXlok3pe3wIKPO9fHGHYlRrftPTcOGWN3d0pkFYVs3sxFj97G1oJTuhcp2e7UjeiWHYGX13gIT8wEkb025sTxvo8EaRaJlJhKqtNJOPHdP9GnxPRRVYAIiA90Gaxgg7TuUVyRTPXW26P2f1CwyJg/p.png?size=178x178&size_mode=1'
     },
     {
         title: "EAUX DE PARFUM",
         buttonText: "Shop Now",
-        link: 'Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
-        image: 'https://uc5f4fec40d5f1cfd6c5466fa1e4.previews.dropboxusercontent.com/p/thumb/ABD9VDyTw5t-6Z29MrXg5TJdUMyUdAgFv4OFe8QLnz6cNT3bWKeN07YhY9PUfy2-oYk2Y0sE1did3mD_pbhrbYMKbgQ3Dp-rzjuYax1tJDgq--9W_eoWPIcbCS2MHEcshc6TF4dUChg3mGDKgJjAFOk1fHdcj_Buk5fIrAsojTCQLo82JzcF8cpk8OEEYGfSsxfkFMIZ1bcWfaucmxXkCBz9dBHqR3J1oYKU0Jqgz9xVuDoMV9PSmr1qa0K2f4FMVD5g_RCSTRXC9Kkw6aXnhMyC4sj-CcYGerUdiuhcdSe7tyscIZV1WrriiLWFVic5hNaePd2UFw5SpkhmEZlC8QUmd3ekhwNu6JrRCtuVCodPoKERaZf59ST_Jr13UhIGopU6WM_XEBZL4IcKk6zdpH6Non73FHAr03g2fwbsK5RtYJdaHWAhBgF9vyopUMbOQ6Kkm9vakCCxsQTN8xeLJz0R/p?fv_content=true&size_mode=5'
+        link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
+        image: 'https://ucf3fd058710ebdd242ac2db0301.previews.dropboxusercontent.com/p/thumb/ABCS8Mp4QFVXC64oHZYyhz0YV-rySBRqnjpsHVlOC6t_eB_JmwEIU3pXpjDlN-J8CdyABPGf2AZgsvUYaJJH9TNbaJqoKnheUq5OxUv6_n0lzRroADu_enLRgS-4fW4CtDRsEq1Ow5HgLfQLoH6bDOuljosv02HGrZgMutpexIqtNjSLgsDPyq3_fz4CkTNuc_jc4lK_tCO2JSZTKfIxvCZA_nuX4uaJmcLWgRfmCvJXnxsupHbGDKduY-sapTsayFQO9Qxsu_TBRl05CYkCRAUYRqxXErCk3jmPMeVtjtsudnwKt8JRUxJ5ShJazd-iwOai5ehnZc98m7uuIvdes_gD4JJh-S6iwLnSKg_2Zdl_5m09dVArc2DUQ3T5FjylIuE_FxUhwCV6xqq_1kXa8VG65n5vw20RQUUdvTtfXhOmwVRwGK_1YTkS93-hdHcobGn2kMsNYGcW9J_KFwSJMjzo_nPabQV8n1u069tgMAVfdg/p.jpeg?size=178x178&size_mode=1'
     },
     {
-        title: "CRÈMES MAINS",
+        title: "CR�MES MAINS",
         buttonText: "Shop Now",
-        link: 'Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
-        image: 'https://ucc4cf31bc4d63e67b490f984181.previews.dropboxusercontent.com/p/thumb/ABC6IChg3PiaPt7VfESvFCeKBkCUjOahrxQwUaR1dmTByRPL61V175UqS50SFLlVtjXRJGQ07GpcGOZME-4Zh0P0h7NHEyaRxkKgr6Zp1deA7DkkoovmiynUi8ptBJc6sdyvXTGFUzYngFPPk1U2i51vS7hnzBYp14hh2m7N-AsHq4cgLY1NYRiLgA_OpBwlQi55KVxCR6U6seI6ztrDXOxLgJ15z0JOa5Q3qCjJsQ_CsSRWceygTyqfUJkIazioCWbgdwzhZlW9L-MsNXMEYbc4rF6TrmEkwOTBuHFTMfn4Qdm6IkZKbFur1avidPRgsgqRXzA87iTMscPh6eBkRr1PZiUQJpOVIwljHEVG2q1Fs3rdYRROWJ4hjYtRYAcBi3eR69lv5OklhkbOdi4rNEcI8sqfa2Qf8WJGUTV6TfIxEYT0djvDswGowZYusduwZx7acGDDMIHMDoi0w57L9KoEhh2HiMaIoKTa4ccC5e_Aqg/p.png?size=2048x1536&size_mode=3'
-    },
+        link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
+        image: 'https://uce895097c8d466d39855b7dffe9.previews.dropboxusercontent.com/p/thumb/ABAU0JO9Z0DeivRjVLVOUSuJCvIhBgJdexdVA40HvNucGh9UDocfXCUXGZOMDb9mQ0AOYS3tBMBwyIO0e8oD2K3S9oI_PcJr8CvOTQHJAAy3PB-7rDrNPJXcZa1jcR604FCbsK-xl52VUlCvPWVti7EoiT_UIu8PCgbISK1hU0ODP73JRBICrTgqjHTTEjTivqtP3vO61_gMBr53y0gaCsZOnt--X5dWQY-Mn1ItOKZwWunPZt2BnxB34aA_C59T9nh0zKBxYDpn9KvowTipwwjBxhiRnJl7mUb0ne9Ouh_AgxDPI72f_5BFPWAHCS4LGhn8Tv4ZmXuYS5Q-CYdtfpV1-BrJbbOJr-8x4XuhJU-JSiqm66EA_VdBk1597A2m7_uoyfc24NnyPhuDBtuBv9DlZKliUJ7lSeDefEnLEFisHOR6dmq8EjdCi5lw4A4pSfovA5JJjzKtO2Zh00XvMKrC7_2h5xXuaHVj_KpWcrd8og/p.png?size=178x178&size_mode=1'
+    }
 ]
 
 var CaruselData = [
-
-
 {
 
     title: '',
-    imageURL: 'https://uc1f99616d1354e821f8fad8685a.previews.dropboxusercontent.com/p/thumb/ABDry4E4zFYZVNpS1T_e2Oi0PhOFXfnieRFYYAdQRbu8RWWHr0cJADYGzMyalPqjD323XNBXAXAOx5AfqKlr5tZsNLKG_iESgKtc6bGmtyzQm2Jeob3dF9sJqWg19xE-aj3TwqJfvpapXQ7chGZqLaamQZxlk_40nem5NqqsWIRL-KKXsIV59gZfWIXQt6Xee7QKDkchxsVTJ9LJUoZ9F0bUvtD7sc2FQdO_U3sodcInnLfmCWzycM61xdc4b5WWbWKwjtZmgWkQNN9nqW2SnOXDJyNQEuiSiCW5BvqVQ1Tvw5d_YVvPeF4BtCeXHAIwPvdMXlt13FozDAmt25Y9wBjNfGyJl_ph0s0AZIYFW3GZ0Q/p.png?fv_content=true&size_mode=5',
+    imageURL: 'https://uc2be454a4c7f8526de23d72996d.previews.dropboxusercontent.com/p/thumb/ABCRZZCA_70PGGrs5NjIJwpEzGerMA3tx813QIIZKBzIYICiHPyfNSVo-bhQwls8HK7sZFf-LnYxOZOiFWd_Vi7IV-7S-Mtu9_Fqn6ntoNK9mZe_OfqTsoRPSpL6C1YwHSWDnRJVVkCNjUkRU18G8HLZPMceMVsBjIlj6vCBygKy14DBlAW3kHDM2Uf6A23f-rvlHH6sSe_YtWqOlcg61Qn4jGEMq8m3E-vP9YtC8eEFjqX-OH083Fz2x85ZADeeTnMkKI_iPK4wwj2-sOMSx7RhDJ2tz5qTs1Lnog4Noi0FwVqaenzzy7-yxHZPH5SMkAPMs_uAMXkTJGIvnh0A4Q-k5DeirrsfWrjZ5UHc1aSWFQ/p.png?size=178x178&size_mode=1',
     description: '',
     buttonText: '',
     time: 5000,
-    deepLink: '/Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+    deepLink: '/Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
     },
 {
 
     title: '',
-    imageURL: 'https://uc75018b652322b31661c2ce6efd.previews.dropboxusercontent.com/p/thumb/ABDocty1p98KFkx7ie1eySHKKirO2BMyr4KpbUMZ9e007_C4JvOBXE74ql4vzYWUbAIwfX2PAEpfUQQgAVbeQ6zbwmD7Jea6o03Kgu7gg2vh7I8ilLkiCY9UMOTMFHPSujeP5dEjEmDnaObx2sx9ulG6-FzxugLF820TSr5ePdL6r5UmzD-nfuKIjNnSK1ProzX9IIdLi_wpdAZqvDEt9OKlthdKlsPM0my8hxxGDjF27G1M5K8_2O2tgR7UbzXghljZuSj6kMlZSwwKD6iXffgjnT7JzMXKXOYlVVk9bKejpExMq_fquKI9XopfPuNlsZDh5r_KupdjKF9MIWBasZVPMcOlIZZ1r9HOkix1aErPmw/p.png?fv_content=true&size_mode=5',
+    imageURL: 'https://ucd67453f8435fd24a9ba3b0200b.previews.dropboxusercontent.com/p/thumb/ABAgMj3PklNSVi69-Yia4br2mL7DCupdFtnhmh6SPIOW9Orf11_FtAF2EhuRcifk_uNFQ8fE3_tbHzpMHEeFwG4dH_s8ixTGh1qwWJ0Z1wtm_9TGVNMp_xJVwOySlwwuCZ93lic9hZiwA1LraRhyU-sjIPjnMti7at65Ak-SBlMhy6eFhT5DmKks9cuDQywAHoTvhElPI60IZcTsC1tgAe4zo75UIxcznuZ_jSdXEoOhe5KmgTg8GpThQtk2TNNrdctytfSFLhC04H-o7flANFyfEhRG2M_4mRi2fV_h2Stu-lO-oCnCfISwNIF0b3BduA1uyXwSJl3vrqE4NILkLdK5wsVG0oa1H4KHcTq22vS56A/p.png?size=178x178&size_mode=1',
     description: '',
     buttonText: '',
     time: 5000,
-    deepLink: '/Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+    deepLink: '/Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
 },
 {
 
     title: '',
-    imageURL: 'https://uc729653f1aa38210bd7b4b1e9fe.previews.dropboxusercontent.com/p/thumb/ABAzehTyUZZ91sst3iGbU5huiFKZX0R4QD3IKgZKtpBdgUpvy43-2TQ7i5Zg2k0ZNqCT8GszHlhOEHqVN6QeW_HhM3ZikDAubYG1hFccjDH9Re_fudbTUwbzSSXOM1L_LollE20nIHwzyF10Vorn52KdBbDwpqr9NDBN-OfLto46bt0152Z2p1BSjAm-KrmOjGvvKCBHRQTT1p6NbW6b799gThht2mmYsWG4M98EBrdnVH7w_Wsk5PB9WdDwm3Hp78Fkc-BrAqRqUbM5H_AbtceQQ1tr705hm-bNP85yjxaDw_iOh4-9XwqdpIvwKBWYI8qfZPZTW39-RlBtgErRi5GddoChXlLYAs80CVnZjNJnkg/p.png?fv_content=true&size_mode=5',
+    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACXSfaElNIoq2d4owfuRSx3a/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/3.BANNI%C3%88RE.png?dl=0',
     description: '',
     buttonText: '',
     time: 5000,
-    deepLink: '/Transactions/scope_items/3a8a9fbfb3c644bf9cc37958c3678880?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+    deepLink: '/Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
 },
 {
 
     title: '',
-    imageURL: 'https://uc714823f110363c1a6847de804d.previews.dropboxusercontent.com/p/thumb/ABChHba2HWZ7b2kY-Wae5oLxl_PRYWtsqGxsXn4GVFsg3uiDYZvLW2CuMGqehioK1Z59RBKpEKhjym9uVclc7NZDPUKNtvFxq_yAekMgVDDs0BNA_JqkMtDsW8MkAg0q_UDao5ZVEi0UO7FqpXz-KjGJ9UbgaP5qExCGRAVx3-6bfKxv0fa4lnAhDnnGySP4IuZ7jn39bs_Q5lgvrG6y4pBxRlUj5nxILb3ZQbCRMp5xeOkh-7Jx5SofQi01GmU9NywK6oXmYvNyBa7HdtnNetmvsFv83PiCmuUAWzgiBA7atc3palQwNzflJRaLW3CzVA4zN2P3k6gNzKfTMywgRBehVh1kaVAvHKAMjofi8oiZ5g/p.png?fv_content=true&size_mode=5',
+    imageURL: 'https://uc5b10bb0460d0257e759f9de15e.previews.dropboxusercontent.com/p/thumb/ABAyMPM5uw3_q2qP7LXlgoSeRBBMTTbG3yW1hQPHVu8RSR8cdkZlZX7Tzfwz9Pca7lTRf_PKOLJ389KzGKFgsfvbGJGZRYdtR1_DP9M748vW_fSPNTSpvcfQdVq_MUCMMgSWFxCPLM_QYAOLlouY2rwBhnOXu7g6Yj3xiySl1_9jwDA2SSP7aDXYUgumEx6pQ2LZwKO21SkSia5iO93W1M1ASi4cJU-OvHJca_NPJmCBYh9_rPNVd1NDm8zyKbbcAtZkUt44aA0ZB41SVmJCWEjazKeBhJIxKnbgz_t4p1T1nUzHBTIvuaGMhKvHzQL-0aEJq5Ohxqtz_jpJ1kZN1J8RFuq_ezmobIVc6cjtP2mCtw/p.png?size=178x178&size_mode=1',
     description: '',
     buttonText: '',
     time: 5000,
