5614dcda53956b16a781e448a7f662c9fbc77afa
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-01-27 13:22:35 +0100

    Update config_header_test.js

--- config_header_test.js
+++ config_header_test.js
@@ -18,7 +18,7 @@ const RightMenu = [
     deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%22%7D%22'
   },
   {
-    title: "Activities",
+    title: "Activit/u000e9s",
     action:"navigation",
     deepLink: 'list/all_activities'
   },
