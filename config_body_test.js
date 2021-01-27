f73d9a06290bc70d22a85675043896cc22a691a1
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-01-27 11:05:55 +0100

    Update config_body_test.js

--- config_body_test.js
+++ config_body_test.js
@@ -41,25 +41,25 @@ var Promotions = [
         title: "MOANA",
         buttonText: "Shop Now",
         link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Paul%20Pitchell%22%5D%7D%5D',
-        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAB9o_LOF7wV-vtDl-eYxPsXa/1-MOANA/COFFRET%20MOANA.jpeg?dl=0'
+        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAB9o_LOF7wV-vtDl-eYxPsXa/1-MOANA/COFFRET%20MOANA.jpeg'
     },
     {
         title: "DELIRIUM FLORAL",
         buttonText: "Shop Now",
         link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
-        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACr7Ls-j6ASshDixikVE1jNa/2-DELIRIUM%20FLORAL/delirium%20floral%20home.png?dl=0'
+        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACr7Ls-j6ASshDixikVE1jNa/2-DELIRIUM%20FLORAL/delirium%20floral%20home.png'
     },
     {
         title: "EAUX DE PARFUM",
         buttonText: "Shop Now",
         link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
-        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AABUT0sMXdWioGkMnAB2fFEta/3-EAUX%20DE%20PARFUM/Pr%C3%A9sentoir%20eau%20parfums.tif?dl=0'
+        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AABUT0sMXdWioGkMnAB2fFEta/3-EAUX%20DE%20PARFUM/Pr%C3%A9sentoir%20eau%20parfums.tif'
     },
     {
         title: "CR\u00c9MES MAINS",
         buttonText: "Shop Now",
         link: 'Transactions/scope_items/3f26ad66-86f7-4661-bd86-6f9c35e684d6?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
-        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAD2eOpASj6seZMUQ7LO2RtYa/4-CR%C3%88MES%20MAINS/P1140485-LIGHT.png?dl=0'
+        image: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAD2eOpASj6seZMUQ7LO2RtYa/4-CR%C3%88MES%20MAINS/P1140485-LIGHT.png'
     }
 ]
 
@@ -67,7 +67,7 @@ var CaruselData = [
 {
 
     title: '',
-    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AABK9C-vIb5wDf4xfaEBPVrja/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/1.%20BANNI%C3%88RE.png?dl=0',
+    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AABK9C-vIb5wDf4xfaEBPVrja/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/1.%20BANNI%C3%88RE.png',
     description: '',
     buttonText: '',
     time: 5000,
@@ -76,7 +76,7 @@ var CaruselData = [
 {
 
     title: '',
-    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACu0u_rPWDxWvj4aXBjz6Sga/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/2.%20BANNI%C3%88RE.png?dl=0',
+    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACu0u_rPWDxWvj4aXBjz6Sga/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/2.%20BANNI%C3%88RE.png',
     description: '',
     buttonText: '',
     time: 5000,
@@ -85,7 +85,7 @@ var CaruselData = [
 {
 
     title: '',
-    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACXSfaElNIoq2d4owfuRSx3a/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/3.BANNI%C3%88RE.png?dl=0',
+    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AACXSfaElNIoq2d4owfuRSx3a/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/3.BANNI%C3%88RE.png',
     description: '',
     buttonText: '',
     time: 5000,
@@ -94,7 +94,7 @@ var CaruselData = [
 {
 
     title: '',
-    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAADEMucCkoV4HQM1-2Y0vQua/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/4.%20BANNIERE.png?dl=0',
+    imageURL: 'https://www.dropbox.com/sh/q633strtdfsizi9/AAADEMucCkoV4HQM1-2Y0vQua/5-BANDEAU%20NOUVEAUT%C3%89/5-BANDEAU%20NOUVEAUT%C3%89/4.%20BANNIERE.png',
     description: '',
     buttonText: '',
     time: 5000,
