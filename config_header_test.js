22c659dfcf73aff6d6492a3a1ae4843707176626
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2021-12-02 17:21:57 +0100

    Update config_header_test.js

--- config_header_test.js
+++ config_header_test.js
@@ -7,7 +7,7 @@ const LeftMenu = [
 ];
 const RightMenu = [
 	
-  {
+  /*{
         catalog: "B2B",
         transaction: "Commande B2B",
     title: "",
@@ -18,7 +18,7 @@ const RightMenu = [
     `,
     action:"setUUIDandNav",
     deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%22%7D%22'
-  },
+  },*/
 	{
 		catalog: "B2B",
         transaction: "Commande B2B",
@@ -30,7 +30,7 @@ const RightMenu = [
     title: "Historique",
     action:"navigation",
     deepLink: 'list/all_activities'
-  },
+  }/*,
 	{
     activity:"Mes contacts",	
     title: "Mes Contacts",
@@ -42,7 +42,7 @@ const RightMenu = [
     title: "Nous contacter",
     action:"createNewActivity",
     deepLink: 'activities/details/{{UUID}}'
-  },
+  },*/
 	
 	
 ]
